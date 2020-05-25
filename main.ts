df24f02c84b4014d3597c1bbc12ad596ca6d0fb4
rotoslinger <harrison.levi.l@gmail.com>
2020-05-25 08:19:03 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,88 @@
+namespace myTiles {
+    //% blockIdentity=images._tile
+    export const tile0 = img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`
+}
+controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
+    music.playTone(311, music.beat(BeatFraction.Whole))
+})
+controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
+    music.playTone(294, music.beat(BeatFraction.Whole))
+})
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    music.playTone(262, music.beat(BeatFraction.Whole))
+})
+controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
+    music.playTone(440, music.beat(BeatFraction.Whole))
+})
+controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
+    music.playTone(392, music.beat(BeatFraction.Whole))
+})
+controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
+    music.playTone(494, music.beat(BeatFraction.Whole))
+})
+let mySprite = sprites.create(img`
+. . . . . . . . . . . . . . . . 
+. . . . . . 6 6 6 6 . . . . . . 
+. . . . 6 6 6 5 5 6 6 6 . . . . 
+. . . 7 7 7 7 6 6 6 6 6 6 . . . 
+. . 6 7 7 7 7 8 8 8 1 1 6 6 . . 
+. . 7 7 7 7 7 8 8 8 1 1 5 6 . . 
+. 6 7 7 7 7 8 8 8 8 8 5 5 6 6 . 
+. 6 7 7 7 8 8 8 6 6 6 6 5 6 6 . 
+. 6 6 7 7 8 8 6 6 6 6 6 6 6 6 . 
+. 6 8 7 7 8 8 6 6 6 6 6 6 6 6 . 
+. . 6 8 7 7 8 6 6 6 6 6 8 6 . . 
+. . 6 8 8 7 8 8 6 6 6 8 6 6 . . 
+. . . 6 8 8 8 8 8 8 8 8 6 . . . 
+. . . . 6 6 8 8 8 8 6 6 . . . . 
+. . . . . . 6 6 6 6 . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, SpriteKind.Player)
+music.setTempo(400)
+music.setVolume(150)
+music.baDing.play()
+mySprite.setFlag(SpriteFlag.StayInScreen, true)
+tiles.setTilemap(tiles.createTilemap(
+            hex`1000100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000`,
+            img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`,
+            [myTiles.tile0],
+            TileScale.Sixteen
+        ))
+forever(function () {
+    pause(150)
+    tiles.placeOnRandomTile(mySprite, myTiles.tile0)
+})
